990a02a4f68ce1b855dec481fe073f58d32dd0cc
bikramia <bikram.rhce@gmail.com>
2024-01-20 11:25:28 -0500

    Selecting Fields to Query

--- playground/views.py
+++ playground/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 from django.db.models import Q, F
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
-from store.models import Product
+from store.models import Product, OrderItem
 
 
 def say_hello(request):
-    # 0, 1, 2, 3, 4
-    # 5,6,7,8,9,10,11,12,13,14,
-    queryset = Product.objects.all()[5:15]
+    #queryset = Product.objects.values_list('id', 'title', 'collection__title') 
+    queryset = Product.objects.filter(id__in=OrderItem.objects.values('product_id').distinct()).order_by('title')
+    
    
 
     return render(request, 'hello.html', {'name': 'Bikram', 'products': list(queryset)})
