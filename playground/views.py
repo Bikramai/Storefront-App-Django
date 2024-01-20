f90da348e6e19b2f0637d1b980cc6a0f67942adc
bikramia <bikram.rhce@gmail.com>
2024-01-20 12:38:40 -0500

    Exercise - Selecting Related Objects

--- playground/views.py
+++ playground/views.py
@@ -2,25 +2,25 @@ from django.shortcuts import render
 from django.db.models import Q, F
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
-from store.models import Product
+from store.models import Product, Order
 
 
-def say_hello(request):
-    # select_related(1)
-    # prefetch_related (n)
-    queryset = Product.objects.prefetch_related(
-        'promotions').select_related('collection').all()
+# def say_hello(request):
+#     # select_related(1)
+#     # prefetch_related (n)
+#     queryset = Product.objects.prefetch_related(
+#         'promotions').select_related('collection').all()
 
-    return render(request, 'hello.html', {'name': 'Bikram', 'products': list(queryset)})
+#     return render(request, 'hello.html', {'name': 'Bikram', 'products': list(queryset)})
 
 
 # Exercise
 # Get the last 5 orders with their customer
 # and items (include product)
-# def say_hello(request):
-#     # select_related(1)
-#     # prefetch_related (n)
-#     queryset = Order.objects.select_relatede(
-#         'customer').order_by('-placed_at')[:5]
+def say_hello(request):
+    # select_related(1)
+    # prefetch_related (n)
+    queryset = Order.objects.select_related(
+        'customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
 
-#     return render(request, 'hello.html', {'name': 'Bikram', 'orders': list(queryset)})
+    return render(request, 'hello.html', {'name': 'Bikram', 'orders': list(queryset)})
