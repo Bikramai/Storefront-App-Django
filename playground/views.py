9f0f23505de3b83d7fb2285a7dabb9a6843acc7a
bikramia <bikram.rhce@gmail.com>
2024-01-20 11:37:00 -0500

    Deferring Fields

--- playground/views.py
+++ playground/views.py
@@ -6,8 +6,7 @@ from store.models import Product, OrderItem
 
 
 def say_hello(request):
-    #queryset = Product.objects.values_list('id', 'title', 'collection__title') 
-    queryset = Product.objects.filter(id__in=OrderItem.objects.values('product_id').distinct()).order_by('title')
+    queryset = Product.objects.defer('description') 
     
    
 
