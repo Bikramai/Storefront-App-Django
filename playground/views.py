506f7a8c31f285cfa7100a39cfa42a1471ad2199
bikramia <bikram.rhce@gmail.com>
2024-01-20 12:18:38 -0500

    Selecting Related Objects

--- playground/views.py
+++ playground/views.py
@@ -2,12 +2,25 @@ from django.shortcuts import render
 from django.db.models import Q, F
 from django.http import HttpResponse
 from django.core.exceptions import ObjectDoesNotExist
-from store.models import Product, OrderItem
+from store.models import Product
 
 
 def say_hello(request):
-    queryset = Product.objects.defer('description') 
-    
-   
+    # select_related(1)
+    # prefetch_related (n)
+    queryset = Product.objects.prefetch_related(
+        'promotions').select_related('collection').all()
 
     return render(request, 'hello.html', {'name': 'Bikram', 'products': list(queryset)})
+
+
+# Exercise
+# Get the last 5 orders with their customer
+# and items (include product)
+# def say_hello(request):
+#     # select_related(1)
+#     # prefetch_related (n)
+#     queryset = Order.objects.select_relatede(
+#         'customer').order_by('-placed_at')[:5]
+
+#     return render(request, 'hello.html', {'name': 'Bikram', 'orders': list(queryset)})
