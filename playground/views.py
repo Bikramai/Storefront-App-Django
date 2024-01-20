057b4cd4849ae7964d4054b1c087abc4c76b4174
bikramia <bikram.rhce@gmail.com>
2024-01-19 20:16:23 -0500

    Managers and QuerySets

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from store.models import Product
 
 
-def calculate():
-    x = 1
-    y = 2
-    return x
 
 
 def say_hello(request):
-    x = calculate()
-    return render(request, 'hello.html', {'name': 'Mosh'})
+    query_set = Product.objects.count()
+
+
+    for product in query_set:
+        print(product)
+
+    return render(request, 'hello.html', {'name': 'Bikram'})
