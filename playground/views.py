b22d029e2e1c3bb559dbac9cbdbabdbc6823ffba
bikramia <bikram.rhce@gmail.com>
2024-01-21 16:24:35 -0500

    Querying Generic Relationships

--- playground/views.py
+++ playground/views.py
@@ -1,16 +1,17 @@
-
-from django.forms import DecimalField
 from django.shortcuts import render
-from django.db.models import Value, F, Func, Count, ExpressionWrapper
-from django.db.models.functions import Concat
-from store.models import Product, Customer
+from django.contrib.contenttypes.models import ContentType
+from store.models import Product
+from tags.models import TaggedItem
 
 
 def say_hello(request):
-    discounted_price = ExpressionWrapper(F('unit_price') * 0.8, output_field=DecimalField())
-    queryset = Product.objects.annotate(
-        discounted_price=discounted_price
+    content_type = ContentType.objects.get_for_model(Product)
+    
+    queryset = TaggedItem.objects \
+        .select_related('tag') \
+        .filter(
+            content_type=content_type,
+            object_id=1
+        )
 
-    )
-
-    return render(request, 'hello.html', {'name': 'Bikram', 'result': list(queryset)})
+    return render(request, 'hello.html', {'name': 'Bikram', 'tags': list(queryset)})
