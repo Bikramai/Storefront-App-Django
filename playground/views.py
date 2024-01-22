ff1dae3764ae6e42e1019146122fae478d67b822
bikramia <bikram.rhce@gmail.com>
2024-01-21 21:44:35 -0500

    Executing Raw SQL Queries

--- playground/views.py
+++ playground/views.py
@@ -1,23 +1,12 @@
 from django.shortcuts import render
-from django.db import transaction
+from django.db import connection
 from store.models import Order, OrderItem, Product, Customer, Collection
 
 
 def say_hello(request):
-    # if we have some code needs to wrap out the code than
-    #........
+    # queryset = Product.objects.raw('SELECT id, title FROM store_product') #use it only while dealing with complex query
 
-    with transaction.atomic():
-        order = Order()
-        order.customer_id = 1
-        order.save()
-
-
-        item = OrderItem()
-        item.order = order
-        item.product_id = -1
-        item.quantity = 1
-        item.unit_price = 10
-        item.save()
-
-    return render(request, 'hello.html', {'name': 'Bikram'})
+    with connection.cursor() as cursor:
+        cursor.execute()               #callproc('get_customers', [1, 2, 'a' ])
+        
+    return render(request, 'hello.html', {'name': 'Bikram', 'reault': list(connection)})
