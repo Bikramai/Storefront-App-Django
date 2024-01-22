68e421ab59b74ee3a686b64f585ff0d2853142db
bikramia <bikram.rhce@gmail.com>
2024-01-21 20:55:45 -0500

    Updating Objects

--- playground/views.py
+++ playground/views.py
@@ -4,11 +4,14 @@ from tags.models import TaggedItem
 
 
 def say_hello(request):
-    collection = Collection()
-    collection.title = 'Video Games'
-    collection.featured_product = Product(pk=1)
-    collection.save()
+    # collection = Collection(pk=11)
+    # collection.title = 'Games'
+    # collection.featured_product = None
+    # collection.save()
 
-    
+
+    Collection.objects.filter(pk=11).update(featured_product=None)
+
+
 
     return render(request, 'hello.html', {'name': 'Bikram'})
